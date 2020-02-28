ff32e7b1071b76715153508f5fc65176f0239a04
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2020-02-27 18:46:35 -0600

    changes to server with cors added

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-const routes = require("./routes/coachesApiRoutes");
 
 const app = express();
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use("/", routes);
+//const routes = require("./routes/coachesApiRoutes");
 
 //connect to mySQL database
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -29,9 +21,45 @@ connection.connect(function (err) {
         console.error("error connecting: " + err.stack);
         return;
     }
-
-    console.log("connected as id " + connection.threadId);
 });
+
+app.use(cors());
+
+// Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+
+  
+// }
+// Add routes, both API and view
+//app.use("/", routes);
+
+console.log("connected as id " + connection.threadId);
+
+app.get("/", (req, res) => {
+    res.send("go to /coaches to see coaches");
+});
+
+
+const SELECT_ALL_PRODUCTS_QUERY = "Select * FROM coaches";
+
+app.get("/coaches", (req, res) => {
+    connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        else {
+            return res.json({
+                data: results
+            });
+        }
+    })
+}
+);
+
 
 app.listen(PORT, function () {
 
